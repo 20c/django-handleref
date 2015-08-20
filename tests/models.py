d0514c0a3ce2d0a54d22df7e79a9193316ea1e39
Matt Griswold <grizz@20c.com>
2015-08-20 18:11:34 +0000

    add created/updated fields

--- tests/models.py
+++ tests/models.py
@@ -8,28 +8,14 @@ class Org(HandleRefModel):
     website = models.URLField(blank=True)
     notes = models.TextField(blank=True)
 
-
-    class Meta:
-        db_table = u'peeringdb_organization'
-        verbose_name_plural = "Organizations"
-
     class HandleRef:
-#        tag = 'hi'
-#        delete_cascade = ["network_set", "facility_set", "ix_set"]
-        delete_cascade = ["widget_set",]
+        #delete_cascade = ["widget_set",]
         tag = 'org'
-        pass
-
-#    _ref_tag = 'org'
 
     def __unicode__(self):
         return self.name
 
+
 class Widget(HandleRefModel):
     name = models.CharField(max_length=255, unique=True)
-
-#    class HandleRef:
-#        tag = 'hi'
-#        delete_cascade = ["network_set", "facility_set", "ix_set"]
-#        pass
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -6,11 +6,10 @@ from tests.models import *
 
 def test_model_init():
     org = Org()
-    assert 'org' == org.ref_tag
+    assert 'org' == org._handleref.tag
     assert 'org' == Org.handleref.tag
     with pytest.raises(ValueError) as e:
         org.handle
 
     widget = Widget()
-
 
