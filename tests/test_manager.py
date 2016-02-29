f7b728bcce0c268b9f08530b9e06894e5c95b5ce
Stefan Pratter <stefan@20c.com>
2016-02-29 14:45:56 +0000

    unit tests

---
+++ tests/test_manager.py
@@ -0,0 +1,59 @@
+import pytest
+import time
+
+from tests.models import *
+from django.test import TestCase
+
+class ManagerTests(TestCase):
+    """
+    Test handle-ref manager functionality
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.initTime = time.time()
+        cls.orgs = []
+        i = 0
+        while i < 10:
+            cls.orgs.append(Org.objects.create(name="Org %d" % i))
+            i = i + 1
+
+        cls.orgs[8].delete()
+        cls.orgs[9].delete(hard=True)
+
+
+    def test_last_change(self):
+        org = self.orgs[8]
+        self.assertEqual(Org.handleref.last_change(), org.updated)
+    
+    def test_since(self):
+        
+        org = self.orgs[0]
+        t = time.time()
+
+        # we wait a second, so we have a valid timestamp to query
+        time.sleep(1)
+
+        # update the org
+        org.name = "Updated name 0"
+        org.save()
+
+        # the org we just updated should be only org in the query set
+        qset = Org.handleref.since(timestamp=t)
+        self.assertEqual(qset.count(), 1)
+        self.assertEqual(qset.first().id, org.id)
+
+
+        # we also want to check that org #8 is in the qset when
+        # the deleted parameter is passed as true
+        qset = Org.handleref.since(timestamp=self.initTime, deleted=True)
+        self.assertIn(self.orgs[8].id, [o.id for o in qset])
+
+        # and that it's missing if we don't
+        qset = Org.handleref.since(timestamp=self.initTime, deleted=True)
+        self.assertIn(self.orgs[8].id, [o.id for o in qset])
+
+    
+    def test_undeleted(self):
+        qset = Org.handleref.undeleted()
+        self.assertNotIn(self.orgs[8].id, [o.id for o in qset])
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,22 +1,45 @@
 
 import pytest
 
+from django.test import TestCase
+
 from tests.models import *
 
 
-def test_model_init():
-    org = Org()
-    assert 'org' == org.ref_tag
-    assert 'org' == Org.handleref.tag
-    with pytest.raises(ValueError) as e:
-        org.handle
+class ModelTests(TestCase):
+    
 
-    widget = Widget()
+    def test_model_init(self):
+        org = Org()
+        self.assertEqual('org', org.ref_tag)
+        self.assertEqual('org', Org.handleref.tag)
+        with self.assertRaises(ValueError) as e:
+            org.handle
 
-    # no tag specified on model, should default to lower-case
-    # class name
-    assert 'widget' == widget.ref_tag 
-    assert 'widget' == Widget.handleref.tag
+        widget = Widget()
 
-    assert widget._handleref.custom_option == "passthrough"
-    assert Widget.handleref.prop("custom_option") == "passthrough"
+        # no tag specified on model, should default to lower-case
+        # class name
+
+        self.assertEqual('widget', widget.ref_tag)
+        self.assertEqual('widget', Widget.handleref.tag)
+
+        self.assertEqual('passthrough', widget._handleref.custom_option)
+        self.assertEqual('passthrough', Widget.handleref.prop("custom_option"))
+
+    def test_soft_delete(self):
+        org = Org.objects.create(name="TEST SOFT DELETE", status="ok")
+        self.assertEqual(org.status, "ok")
+        org.delete()
+        org.refresh_from_db()
+        self.assertEqual(org.status, "deleted")
+
+
+    def test_hard_delete(self):
+        org = Org.objects.create(name="TEST HARD DELETE", status="ok")
+        self.assertEqual(org.status, "ok")
+        org.delete(hard=True)
+        with self.assertRaises(Org.DoesNotExist) as e:
+            org.refresh_from_db()
+
+       
