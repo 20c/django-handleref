d15255700a6c7fcd4642d0debb9793e25784aa0f
Stefan Pratter <stefan@20c.com>
2016-02-29 14:45:29 +0000

    fix indentation

--- django_handleref/rest/serializers.py
+++ django_handleref/rest/serializers.py
@@ -2,12 +2,12 @@ from django_handleref.models import HandleRefModel
 from rest_framework import serializers
 
 class HandleRefSerializer(serializers.ModelSerializer):
-  version = serializers.ReadOnlyField()
-  status = serializers.ReadOnlyField()
-  class Meta:
-    model = HandleRefModel,
-    fields = [
-      'created',
-      'updated',
-      'status'
-    ]
+    version = serializers.ReadOnlyField()
+    status = serializers.ReadOnlyField()
+    class Meta:
+        model = HandleRefModel
+        fields = [
+            'created',
+            'updated',
+            'status'
+        ]
