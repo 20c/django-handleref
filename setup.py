9d80f1037b5aa563376ae7ad46cbdbf99da1587e
Matt Griswold <grizz@20c.com>
2017-09-13 17:04:13 +0000

    add py versions

--- setup.py
+++ setup.py
@@ -13,6 +13,13 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=[
